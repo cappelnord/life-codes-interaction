7707bd7477df47831b37a0d7e46d64de7a0607ad
Patrick Borgeat <patrick@borgeat.de>
2025-05-12 15:25:30 +0300

    make sure that we actually store an integer as an code block argument.

--- interaction/scripts/code_block/code_block_argument.gd
+++ interaction/scripts/code_block/code_block_argument.gd
@@ -12,7 +12,12 @@ var type: Type
 func _init(parameter: CodeBlockParameter, type: Type, value):
 	self.parameter = parameter
 	self.type = type
-	self.value = value
+	
+	# make sure that we actually store an integer here
+	if parameter.type == CodeBlockParameter.Type.INTEGER:
+		self.value = int(value)
+	else:
+		self.value = value
 
 func duplicate() -> CodeBlockArgument:
 	return CodeBlockArgument.new(parameter, type, value)
